8ba5a785edb14e6f46f7281462145b6b136a827f
Aarush S. <70100429+aarushsonker@users.noreply.github.com>
2025-02-21 20:22:36 +0530

    Source Code for the Program

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the program name to set RAM limit:");
+        string programName = Console.ReadLine();
+
+        Console.WriteLine("Enter the RAM limit in MB:");
+        int ramLimit = Convert.ToInt32(Console.ReadLine());
+
+        Process[] processes = Process.GetProcessesByName(programName);
+
+        foreach (Process process in processes)
+        {
+            if (process.WorkingSet64 > ramLimit * 1024 * 1024)
+            {
+                process.Kill();
+                Console.WriteLine($"{programName} exceeded the RAM limit of {ramLimit}MB and has been closed.");
+            }
+        }
+    }
+}
